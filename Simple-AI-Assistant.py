382bc75af96a18e9a85a2071d3b23d74c538d549
ShadowEmperorYT <76802372+ShadowEmperorYT@users.noreply.github.com>
2020-12-31 12:58:51 +0530

    Add files via upload

---
+++ Simple-AI-Assistant.py
@@ -0,0 +1,10 @@
+import wikipedia
+
+print("This program is still under developing some new feautures will available in next version some feautures are update checker,sapi5 voice.")
+user=input("How can i help you: ")
+print(user)
+print("Searching in wikipedia")
+
+print(wikipedia.summary(user))
+
+
